992ff8fa828d8c7aa80bf98f07b7f1824b4e5825
namevug <ciavug@gmail.com>
2023-04-24 14:19:05 -0300

    adjust: HeaderComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Header } from './Components/Header'
+import { Header } from './Components/Header/Header'
 import './styles/global.css'
 
 export function App() {
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import { AiOutlinePlusCircle } from 'react-icons/ai'
+import todoLogo from '../../assets/Logo.svg'
+import styles from './Header.module.css'
+
+export function Header() {
+  return (
+    <>
+      <header className={styles.header}>
+        <img src={todoLogo} alt='ToDo Logo' />
+
+        <form className={styles.newTask}>
+          <input type='text' placeholder='Adicione uma nova tarefa' />
+          <button>
+            Criar
+            <AiOutlinePlusCircle size={20} />
+          </button>
+        </form>
+      </header>
+    </>
+  )
+}

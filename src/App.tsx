0495a62fd649d533ff8402b27b0fb43bb05b224e
namevug <ciavug@gmail.com>
2023-04-25 23:53:43 -0300

    feat: CommitDay

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react'
 import { Header } from './Components/Header/Header'
 import { Tasks } from './Components/Tasks/Index'
 import './styles/global.css'
 
+export interface Card {
+  id: string
+  title: string
+  isCompleted: boolean
+}
+
 export function App() {
+  const [tasks, setTask] = useState<Card[]>([])
+
   return (
     <>
       <div>
         <Header />
-        <Tasks />
+        <Tasks tasks={tasks} />
       </div>
     </>
   )

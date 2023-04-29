ffbc11facf225bba7a74ded4b824925065d5e107
namevug <ciavug@gmail.com>
2023-04-28 23:46:14 -0300

    feat: State of the number of tasks

--- src/Components/Tasks/Index.tsx
+++ src/Components/Tasks/Index.tsx
@@ -1,29 +1,34 @@
-import { iCard } from '../../App'
+import { Task } from '../../App'
 import { Card } from '../Card/Index'
 import styles from './Tasks.module.css'
 
 interface Props {
-  tasks: iCard[]
+  tasks: Task[]
 }
 
 export function Tasks({ tasks }: Props) {
+  const tasksQuantity = tasks.length
+  const completedTasks = tasks.filter((task) => task.isCompleted).length
+
   return (
     <>
       <section className={styles.tasks}>
         <header className={styles.header}>
           <div>
             <p>Tarefas criadas</p>
-            <span>5</span>
+            <span>{tasksQuantity}</span>
           </div>
 
           <div>
             <p className={styles.completed}>Concluídas</p>
-            <span>2 de 5</span>
+            <span>
+              {completedTasks} de {tasksQuantity}
+            </span>
           </div>
         </header>
         <div className={styles.cards}>
-          {tasks.map((tasks) => {
-            return <Card />
+          {tasks.map((task) => {
+            return <Card key={task.id} task={task} />
           })}
         </div>
       </section>

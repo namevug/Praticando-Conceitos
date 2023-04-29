be702a779400f9b0fe0df49ab3a4908178753398
namevug <ciavug@gmail.com>
2023-04-28 23:47:07 -0300

    feat: Rename component

---
+++ src/Components/Header/Index.tsx
@@ -0,0 +1,44 @@
+import { AiOutlinePlusCircle } from 'react-icons/ai'
+import todoLogo from '../../assets/Logo.svg'
+import styles from './Header.module.css'
+import { ChangeEvent, FormEvent, useState } from 'react'
+
+interface Props {
+  onAddTask: (taskTitle: string) => void
+}
+
+export function Header({ onAddTask }: Props) {
+  const [title, setTitle] = useState('')
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    onAddTask(title)
+    setTitle('')
+  }
+
+  function onChangeTitle(event: ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value)
+  }
+
+  return (
+    <>
+      <header className={styles.header}>
+        <img src={todoLogo} alt='ToDo Logo' />
+
+        <form className={styles.newTask} onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Adicione uma nova tarefa'
+            onChange={onChangeTitle}
+            value={title}
+          />
+          <button>
+            Criar
+            <AiOutlinePlusCircle size={20} />
+          </button>
+        </form>
+      </header>
+    </>
+  )
+}
